d310d3507d00052b6fae70af3e6f94125245541b
thomxsnguyen <thomasn8255@gmail.com>
2024-09-05 13:15:57 -0700

    made shuffling faster

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ app = Flask(__name__)
 app.secret_key = config.CLIENT_SECRET
 app.config['SESSION_COOKIE_NAME'] = 'spotify-login-session'
 
-
 sp_oauth = SpotifyOAuth(
     client_id=config.CLIENT_ID,
     client_secret=config.CLIENT_SECRET,
@@ -35,6 +34,21 @@ def get_token():
 
     return token_info['access_token']
 
+def get_all_albums(sp, artist_id):
+    albums = []
+    results = sp.artist_albums(artist_id, album_type='album,single,compilation', limit=50)
+    albums.extend(results['items'])
+    while results['next']:
+        results = sp.next(results)
+        albums.extend(results['items'])
+    return albums
+
+def get_tracks_from_albums(sp, albums):
+    all_tracks = []
+    for album in albums:
+        tracks = sp.album_tracks(album['id'])['items']
+        all_tracks.extend(tracks)
+    return all_tracks
 
 @app.route('/')
 def login():
@@ -64,7 +78,6 @@ def callback():
 
     return redirect('/select_artists')
 
-
 @app.route('/select_artists')
 def select_artists():
     selected_artists = session.get('selected_artists', [])
@@ -88,15 +101,6 @@ def search_artists():
 
     return {'artists': [{'id': artist['id'], 'name': artist['name']} for artist in artists]}
 
-def get_all_albums(sp, artist_id):
-    albums = []
-    results = sp.artist_albums(artist_id, album_type='album,single,compilation')
-    albums.extend(results['items'])
-    while results['next']:
-        results = sp.next(results)
-        albums.extend(results['items'])
-    return albums
-
 @app.route('/shuffle', methods=['POST'])
 def shuffle():
     selected_artist_ids = request.form.getlist('artists')
@@ -117,14 +121,16 @@ def shuffle():
     device_id = devices['devices'][0]['id']
 
     all_tracks = []
-    for artist_id in selected_artist_ids:
-        try:
-            albums = get_all_albums(sp, artist_id)
-            for album in albums:
-                tracks = sp.album_tracks(album['id'])['items']
+    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+        future_to_artist = {executor.submit(get_all_albums, sp, artist_id): artist_id for artist_id in selected_artist_ids}
+        for future in concurrent.futures.as_completed(future_to_artist):
+            artist_id = future_to_artist[future]
+            try:
+                albums = future.result()
+                tracks = get_tracks_from_albums(sp, albums)
                 all_tracks.extend(tracks)
-        except spotipy.exceptions.SpotifyException as e:
-            print(f"Error retrieving albums or tracks for artist {artist_id}: {e}")
+            except Exception as e:
+                print(f"Error retrieving albums or tracks for artist {artist_id}: {e}")
 
     if all_tracks:
         random.shuffle(all_tracks)
@@ -171,7 +177,6 @@ def current_track():
         }
     else:
         return {'show_controls': False}  
-
 
 @app.route('/previous')
 def previous_track():
